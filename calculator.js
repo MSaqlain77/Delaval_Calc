84d7a4805a80b40be1e32a9184a4ea0f0eb43166
MSaqlain77 <91142648+MSaqlain77@users.noreply.github.com>
2024-07-13 23:22:37 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -21,25 +21,15 @@ function calculateLinerChange() {
     const resultElement = document.getElementById('resultLinerReplacement');
     resultElement.innerHTML = `Each cluster milks ${cowsPerCluster.toFixed(2)} cows. You should change the liner every ${usesPerCluster.toFixed(2)} uses or 6 months, whichever comes first.`;
 }
-/*function calculateLinerChange() {
-    const numCows = document.getElementById('numCows').value;
-    const numClusters = document.getElementById('numClusters').value;
-    const resultElement = document.getElementById('result');
-    const maxDays = 180;  // 6 months
-
-    if (numCows > 0 && numClusters > 0) {
-        const cowsPerCluster = numCows / numClusters;
-        const daysToReplace = 2500 / cowsPerCluster;
-        const replacementTimeInDays = Math.min(daysToReplace, maxDays);
-        const replacementTimeInMonths = replacementTimeInDays / 30;
-
-        const replacementTimeMessage = (replacementTimeInDays === daysToReplace)
-            ? `${replacementTimeInDays.toFixed(2)} days (${replacementTimeInMonths.toFixed(2)} months)`
-            : `180 days (6 months)`;
-
-        resultElement.innerText = `Number of cows milked per cluster: ${cowsPerCluster.toFixed(2)}
-        \nYou need to replace the liner in ${replacementTimeMessage}.`;
-    } else {
-        resultElement.innerText = 'Please enter valid numbers for cows and clusters.';
-    }*\
-
+// Function to calculate average milking time for the average milking time calculator
+function calculateAverageMilkingTime() {
+    const numCows = document.getElementById('numCowsTime').value;
+    const numBuckets = document.getElementById('numBuckets').value;
+    const numHandMilkers = document.getElementById('numHandMilkers').value || 0;
+    const totalMilkingUnits = parseInt(numBuckets) + parseInt(numHandMilkers);
+    const cowsPerUnit = numCows / totalMilkingUnits;
+    const minMilkingTime = cowsPerUnit * 7.5;
+    const maxMilkingTime = cowsPerUnit * 8.5;
+    const resultElement = document.getElementById('resultAverageMilkingTime');
+    resultElement.innerHTML = `The average milking time per unit is between ${minMilkingTime.toFixed(2)} and ${maxMilkingTime.toFixed(2)} minutes.`;
+}
